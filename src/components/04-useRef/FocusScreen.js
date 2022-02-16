9011367383a53b9be4ed69485c263ccba1fa0968
jonathan <jonathancggs@gmail.com>
2022-02-15 20:07:42 -0600

    :jigsaw: Components
    updating the component to add funcionality and use the new hook useRef

--- src/components/04-useRef/FocusScreen.js
+++ src/components/04-useRef/FocusScreen.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import "../../styles/generals.css";
 
 export const FocusScreen = () => {
+
+  const inputRef = useRef();
+  console.log(inputRef);
+
+  const handleClick = () => {
+    inputRef.current.select();
+  }
+
   return (
-    <div>FocusScreen</div>
-  )
+    <>
+      <h1>FocusScreen</h1>
+      <hr />
+
+      <input 
+        ref={ inputRef }
+        className='form-control mb-2'
+        placeholder='Nombre'
+      />
+
+      <button className='btn btn-outline-primary mt-2'
+      onClick={handleClick}>
+        Focus
+      </button>
+    </>
+  );
 }
