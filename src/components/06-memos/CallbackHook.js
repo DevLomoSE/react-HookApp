133e1239a6dceb5c962319e30e4ef80f5c33eaee
jonathan <jonathancggs@gmail.com>
2022-02-16 13:00:10 -0600

    :jigsaw: Components
    creating the component to use useCallback hook

---
+++ src/components/06-memos/CallbackHook.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../styles/generals.css';
+
+export const CallbackHook = () => {
+  return (
+    <>
+      <h1>CallbackHook</h1>
+      <hr/>
+    </>
+  );
+}
