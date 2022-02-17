325d0365493a9a66acca1a0d8d19b58fe65b0ccc
jonathan <jonathancggs@gmail.com>
2022-02-16 21:34:43 -0600

    :sparkles: ToDoApp
    adding the functionallity to the useReducer and all the logic for the ToDoApp

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,27 +1,87 @@
-import React, { useReducer } from 'react';
-import '../../styles/generals.css';
-import { todoReducer } from './todoReducer';
+import React, { useEffect, useReducer } from "react";
+import { useForm } from "../../hooks/useForm";
+import "../../styles/todo.css";
+import { todoReducer } from "./todoReducer";
 
-const initialState = [{
-    id: crypto.randomUUID(),
-    desc: 'Aprender react',
-    done: false
-}]
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
 
 export const TodoApp = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
 
-    const [ todos ] = useReducer(todoReducer, initialState);
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: "",
+  });
 
-    console.log(todos);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (description.trim().length <= 0) return;
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      desc: description,
+      done: false,
+    };
+
+    const addTodo = {
+      type: "add",
+      payload: newTodo,
+    };
+
+    dispatch(addTodo);
+    reset();
+  };
 
   return (
     <>
-      <h1>TodoApp</h1>
+      <h1>TodoApp ( {todos.length} )</h1>
       <hr />
 
-      <ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, i) => (
+              <li key={todo.id} className="list-group-item">
+                <p className="text-center">
+                  {i + 1}. {todo.desc}
+                </p>
+                <button className="btn btn-outline-danger">Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
 
-      </ul>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="description"
+              className="form-control"
+              placeholder="ToDo"
+              autoComplete="off"
+              value={description}
+              onChange={handleInputChange}
+            />
+            <div className="d-grid gap-2">
+              <button
+                type="submit"
+                className="btn btn-outline-primary btn-block mt-1"
+              >
+                Agregar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
-}
+};
